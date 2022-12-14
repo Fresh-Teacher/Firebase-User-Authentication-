29935cd44257569ee9cb6e73402d51f222243f86
Fresh-Teacher <98003374+Fresh-Teacher@users.noreply.github.com>
2022-12-14 03:12:02 -0800

    Initial commit

---
+++ TabianConsulting/app/src/main/java/courses/pluralsight/com/tabianconsulting/LoginActivity.java
@@ -0,0 +1,35 @@
+package courses.pluralsight.com.tabianconsulting;
+
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private static final String TAG = "LoginActivity";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
